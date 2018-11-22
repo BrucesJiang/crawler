d0e0eba7460425927b4772df3f1d94f2c5a12db6
BrucesJiang <1948871867@qq.com>
2018-11-22 12:15:45 +0800

    构建前端

---
+++ concurrent/frontend/view/template_test.go
@@ -0,0 +1,45 @@
+package view
+
+import (
+	"crawler/concurrent/engine"
+	"crawler/concurrent/frontend/model"
+	common "crawler/concurrent/model"
+	template2 "html/template"
+	"os"
+	"testing"
+)
+
+func TestTemplate(t *testing.T) {
+
+	 template := template2.Must(
+	 	template2.ParseFiles("template.html"))
+
+	 out, err := os.Create("template_test.html")
+	 page := model.SearchResult{}
+	 page.Hits = 123
+	 //page.Start = 1
+	item := engine.Item{
+		Url: "http://album.zhenai.com/u/1902329077",
+		Type: "zhenai",
+		Id: "1902329077",
+		Payload: common.Profile{
+			Name: "麦甜",
+			Age: 28,
+			Gender: "女士",
+			Height: 155,
+			Income: "5-8千",
+			Marriage: "丧偶 ",
+			XingZuo: "天秤座",
+			WorkPlace: "阿坝红原",
+		},
+	}
+
+	for i := 0; i < 10; i ++ {
+		page.Items = append(page.Items, item)
+	}
+	 //err := template.Execute(os.Stdout, page)
+	 err = template.Execute(out, page)
+	 if err != nil {
+	 	panic(err)
+	 }
+}
